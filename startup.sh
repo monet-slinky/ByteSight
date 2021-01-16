abf2e7a6cf11b71a7fa087f43cdb6fa4ac5206d6
Zzcool8 <45108550+Zzcool8@users.noreply.github.com>
2021-01-15 19:13:46 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-apt update && apt install -y libglib2.0-0 libsm6 libxext6 libxrender-dev
+apt update && apt install -y libglib2.0-0 libgthread-2.0.so.0 libglib-2.0.so.0 libsm6 libxext6 libxrender-dev
 pip install --upgrade pip
-pip uninstall opencv-python
-pip install opencv-python
 pip install opencv-python-headless
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
