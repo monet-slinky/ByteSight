a70f0cd482e6ba2f7e0e242442a88bc55ed962eb
Zzcool8 <45108550+Zzcool8@users.noreply.github.com>
2021-01-20 20:09:18 -0500

    apt install libgl1-mesa-glx added

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 apt update && apt install -y libglib2.0-0 libgthread-2.0.so.0 libglib-2.0.so.0 libsm6 libxext6 libxrender-dev
 pip install --upgrade pip
 pip install opencv-python-headless
+apt install libgl1-mesa-glx
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
