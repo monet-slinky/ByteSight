21c7cc4443030ecfa22d0478d9b1794a416315b9
Zzcool8 <45108550+Zzcool8@users.noreply.github.com>
2021-01-20 20:31:28 -0500

    install opencv-contrib-python-headless

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt update && apt install -y libglib2.0-0 libgthread-2.0.so.0 libglib-2.0.so.0 libsm6 libxext6 libxrender-dev
 pip install --upgrade pip
-pip install opencv-python-headless
-apt install libgl1-mesa-glx
+pip install opencv-contrib-python-headless
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
